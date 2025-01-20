590fd503946f98088f57a460159f1b6aecb222b0
piyushsinghania <piyushsinghania28@gmail.com>
2025-01-20 13:26:58 +0530

    Added default values for method arguments

--- binarySearch.js
+++ binarySearch.js
@@ -1,14 +1,14 @@
-const binarySearch = ({ key, start, end, array }) => {
+const binarySearch = ({ key, array, start = 0, end = array.length - 1 }) => {
   if (!(start <= end)) return -1;
 
   const middle = Math.floor((start + end) / 2);
 
   if (key < array[middle]) {
-    return binarySearch({ key, start, end: middle - 1, array });
+    return binarySearch({ key, array, start, end: middle - 1 });
   }
 
   if (key > array[middle]) {
-    return binarySearch({ key, start: middle + 1, end, array });
+    return binarySearch({ key, array, start: middle + 1, end });
   }
 
   return middle;
@@ -16,9 +16,9 @@ const binarySearch = ({ key, start, end, array }) => {
 
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const index = binarySearch({
-  key: 8,
+  key: 2,
+  array: array,
   start: 0,
   end: array.length - 1,
-  array: array,
 });
 console.log('index', index);
