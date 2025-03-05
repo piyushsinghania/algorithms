049b5d8fed5fe92154befb2ec7a1769e0a5e4cbe
piyushsinghania <piyushsinghania28@gmail.com>
2025-03-05 18:13:40 +0530

    Optimised bubble sort - best case scenario

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,9 +1,17 @@
 const bubbleSort = (arr) => {
   for (let i = arr.length - 1; i > 0; i--) {
+    let swappedValues = false;
+
     for (let j = 0; j < i; j++) {
+      iterationCount++;
       if (arr[j] > arr[j + 1]) {
         [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
+        swappedValues = true;
       }
+    }
+
+    if (!swappedValues) {
+      break;
     }
   }
 
