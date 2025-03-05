61b4343af7a50ee7ce0a789a005d077c093bc2fa
piyushsinghania <piyushsinghania28@gmail.com>
2025-03-05 18:31:22 +0530

    Selection sort

---
+++ selectionSort.js
@@ -0,0 +1,20 @@
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minItemIndex = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[minItemIndex] > arr[j]) {
+        minItemIndex = j;
+      }
+    }
+
+    if (i !== minItemIndex) {
+      [arr[i], arr[minItemIndex]] = [arr[minItemIndex], arr[i]];
+    }
+  }
+
+  return arr;
+}
+
+const arr = [10, 1, 5, 7, 9, 3, 8, 4, 6, 2];
+console.log(selectionSort(arr));
