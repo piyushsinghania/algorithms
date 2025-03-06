2cb720710cb9e90ce046a5fa8453a76aa52d1541
piyushsinghania <piyushsinghania28@gmail.com>
2025-03-06 14:18:01 +0530

    Merge sort

---
+++ mergeSort.js
@@ -0,0 +1,40 @@
+const mergeTwoSortedArrays = (arrayOne, arrayTwo) => {
+  let i = 0;
+  let j = 0;
+
+  const mergedArray = [];
+
+  while (i < arrayOne.length && j < arrayTwo.length) {
+    if (arrayOne[i] < arrayTwo[j]) {
+      mergedArray.push(arrayOne[i]);
+      i++;
+    } else {
+      mergedArray.push(arrayTwo[j]);
+      j++;
+    }
+  }
+
+  while (i < arrayOne.length) {
+    mergedArray.push(arrayOne[i]);
+    i++;
+  }
+
+  while (j < arrayTwo.length) {
+    mergedArray.push(arrayTwo[j]);
+    j++;
+  }
+
+  return mergedArray;
+}
+
+const mergeSort = (array) => {
+  if (array.length <= 1) return array;
+
+  const mid = Math.floor(array.length / 2);
+  const leftSortedArray = mergeSort(array.slice(0, mid));
+  const rightSortedArray = mergeSort(array.slice(mid))
+  return mergeTwoSortedArrays(leftSortedArray, rightSortedArray);
+}
+
+const array = [3, 1, 5, 8, 9, 7, 4, 6, 2];
+console.log(mergeSort(array))
