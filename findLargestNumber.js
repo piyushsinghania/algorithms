63921e35d9f5148af1a75571fc6ac7222120ff3a
piyushsinghania <piyushsinghania28@gmail.com>
2025-03-06 14:34:43 +0530

    Find largest number - divide and conquer

---
+++ findLargestNumber.js
@@ -0,0 +1,17 @@
+const findLargestNumber = (array) => {
+  if (array.length === 0) return 0;
+  if (array.length === 1) return array[0];
+
+  const mid = Math.floor(array.length / 2);
+  const largestLeftNumber = findLargestNumber(array.slice(0, mid));
+  const largestRightNumber = findLargestNumber(array.slice(mid));
+
+  if (largestLeftNumber > largestRightNumber) {
+    return largestLeftNumber;
+  } else {
+    return largestRightNumber;
+  }
+}
+
+const array = [3, 1, 5, 8, 9, 10, 4, 6, 2, 7];
+console.log(findLargestNumber(array));
