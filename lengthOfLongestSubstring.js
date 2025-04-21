9794a4ad12f6b11d8158d27facbc33e683f8087c
piyushsinghania <piyushsinghania28@gmail.com>
2025-04-21 18:49:26 +0530

    Solved lengthOfLongestSubstring problem in O(n) time complexity

--- lengthOfLongestSubstring.js
+++ lengthOfLongestSubstring.js
@@ -4,37 +4,60 @@ const isUnique = (str) => {
 
 // Sliding Window Approach
 // Time Complexity - O(n^2) - Quadratic - Because of slice method
+// const lengthOfLongestSubstring = (str) => {
+//   if (str.length <= 1) return str.length;
+
+//   let left = 0;
+//   let maxWindowSize = 1;
+
+//   for (let right = 1; right <= str.length; right++) {
+//     const substring = str.slice(left, right);
+//     if (isUnique(substring)) {
+//       maxWindowSize = Math.max(substring.length, maxWindowSize);
+//     } else {
+//       left++;
+//     }
+//   }
+
+//   return maxWindowSize;
+// };
+
+// Sliding Window Approach
+// Time Complexity - O(n) - Linear - Using a charIndex
 const lengthOfLongestSubstring = (str) => {
   if (str.length <= 1) return str.length;
 
-  let left = 0;
-  let maxWindowSize = 1;
+  let maxWindowSize = 0;
+  let windowStart = 0;
+  let charIndex = {};
 
-  for (let right = 1; right <= str.length; right++) {
-    const substring = str.slice(left, right);
-    if (isUnique(substring)) {
-      maxWindowSize = Math.max(substring.length, maxWindowSize);
-    } else {
-      left++;
+  for (let end = 0; end < str.length; end++) {
+    const char = str[end];
+
+    if (char in charIndex && charIndex[char] >= windowStart) {
+      windowStart = charIndex[char] + 1;
     }
+
+    charIndex[char] = end;
+    maxWindowSize = Math.max(maxWindowSize, end - windowStart + 1);
   }
 
   return maxWindowSize;
 };
 
-console.log('pbbabcbadb', lengthOfLongestSubstring('pbbabcbadb'));
-console.log('abcabcbb', lengthOfLongestSubstring('abcabcbb'));
-console.log('bbbbb', lengthOfLongestSubstring('bbbbb'));
-console.log('pwwkew', lengthOfLongestSubstring('pwwkew'));
-console.log('', lengthOfLongestSubstring(''));
-console.log('a', lengthOfLongestSubstring('a'));
-console.log('abcd', lengthOfLongestSubstring('abcd'));
-console.log('dvdf', lengthOfLongestSubstring('dvdf'));
-console.log('tmmzuxt', lengthOfLongestSubstring('tmmzuxt'));
-console.log('anviaj', lengthOfLongestSubstring('anviaj'));
-console.log('abcdefga', lengthOfLongestSubstring('abcdefga'));
-console.log('aabbccddeeff', lengthOfLongestSubstring('aabbccddeeff'));
+console.log('pbbabcbadb', lengthOfLongestSubstring('pbbabcbadb')); // 4
+console.log('abcabcbb', lengthOfLongestSubstring('abcabcbb')); // 3
+console.log('bbbbb', lengthOfLongestSubstring('bbbbb')); // 1
+console.log('pwwkew', lengthOfLongestSubstring('pwwkew')); // 3
+console.log('', lengthOfLongestSubstring('')); // 0
+console.log('a', lengthOfLongestSubstring('a')); // 1
+console.log('abcd', lengthOfLongestSubstring('abcd')); // 4
+console.log('dvdf', lengthOfLongestSubstring('dvdf')); // 3
+console.log('tmmzuxt', lengthOfLongestSubstring('tmmzuxt')); // 5
+console.log('anviaj', lengthOfLongestSubstring('anviaj')); // 5
+console.log('abcdefga', lengthOfLongestSubstring('abcdefga')); // 7
+console.log('aabbccddeeff', lengthOfLongestSubstring('aabbccddeeff')); // 2
 console.log(
   'abcdefghijklmnopqrstuvwxyz',
   lengthOfLongestSubstring('abcdefghijklmnopqrstuvwxyz')
-);
+); // 26
